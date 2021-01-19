4c9d673cf9592d0096e85ba374fe8a112e45f92e
Andrew Klinsic <andrewklinsic@gmail.com>
2021-01-19 04:00:36 -0500

    Added a find all items function to ItemDB

--- Assets/Scripts/Inventory/ItemDatabase.cs
+++ Assets/Scripts/Inventory/ItemDatabase.cs
@@ -1,19 +1,40 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace Spire.Inventory
 {
+    [ExecuteInEditMode]
     public class ItemDatabase : MonoBehaviour
     {
-        public List<Item> items = new List<Item>();
+        public bool updateItemDatabase;
+        public List<Item> itemList = new List<Item>();
         void Awake()
         {
-           
+
         }
 
-        //public BaseItem GetItemByID(int id)
-        //{
-        //    return items.Find(item => item.id == id);
-        //}
+        /// <summary>
+        /// Called when the script is loaded or a value is changed in the
+        /// inspector (Called in the editor only).
+        /// </summary>
+        void OnValidate()
+        {
+            if (updateItemDatabase)
+                UpdateDB();
+        }
+
+        private void UpdateDB()
+        {
+            string[] assetNames = AssetDatabase.FindAssets("t:Item", new[] { "Assets/Item_ScriptableObjects" });
+            itemList.Clear();
+            foreach (string SOName in assetNames)
+            {
+                string SOPath = AssetDatabase.GUIDToAssetPath(SOName);
+                Item item = AssetDatabase.LoadAssetAtPath<Item>(SOPath);
+                itemList.Add(item);
+            }
+        }
+
     }
 }

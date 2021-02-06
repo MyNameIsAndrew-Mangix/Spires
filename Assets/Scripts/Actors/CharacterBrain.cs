551196e0c2ef2e49b4f5c7d8fb7198add004b20a
Andrew Klinsic <andrewklinsic@gmail.com>
2021-02-05 20:21:08 -0500

    CharacterBrain no longer monobh. added iActorMover

--- Assets/Scripts/Actors/CharacterBrain.cs
+++ Assets/Scripts/Actors/CharacterBrain.cs
@@ -2,8 +2,10 @@
 
 namespace Spire.Actors
 {
-    public abstract class CharacterBrain : MonoBehaviour
+    public abstract class CharacterBrain
     {
+        protected IActorMover iActorMover;
         public abstract void Tick();
+        public abstract void GetMovingActor(IActorMover actorMover);
     }
 }
